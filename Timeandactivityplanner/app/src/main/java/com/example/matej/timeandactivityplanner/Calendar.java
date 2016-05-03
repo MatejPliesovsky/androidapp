adb6529d5264350b3b85d462564dc53a7ed355b0
tomastomcik <tomas.tomcik@akademiasovy..sk>
2016-05-03 09:06:45 +0200

    Updated calendar

--- Timeandactivityplanner/app/src/main/java/com/example/matej/timeandactivityplanner/Calendar.java
+++ Timeandactivityplanner/app/src/main/java/com/example/matej/timeandactivityplanner/Calendar.java
@@ -1,24 +1,50 @@
 package com.example.matej.timeandactivityplanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.Toast;
 
-// thos method create Calendar
-public class Calendar extends AppCompatActivity {
-
+public class Calendar extends ActionBarActivity {
+    CalendarView calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        // this can be add to make calendar
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new OnDateChangeListener(){
 
-        CalendarView calendar = (CalendarView) findViewById(R.id.calendar);
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            public void onSelectedDayChange(CalendarView view,
+                                            int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(),
+                        dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calendar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
